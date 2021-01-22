6dc073f85827158c995c2be6a2c38cf122f96aea
Adilet <dylan9803@mail.ru>
2021-01-22 17:40:50 +0600

    'addingParsingButton'

--- justapp/urls.py
+++ justapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, urlparser
 
 urlpatterns = [
-	path('', index, name='main')
+	path('', index, name='main'),
+	path('actparsing/', urlparser, name='actparsing'),
 ]
--- justapp/views.py
+++ justapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import UrlTimer, Result
 
-# Create your views here.
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+import requests 
+from bs4 import BeautifulSoup as BS
+
+
+
 def index(request):
 	allurls = UrlTimer.objects.all()
 	results = Result.objects.all()
@@ -11,3 +18,25 @@ def index(request):
 	return render(request, 'index.html', {'allurls': allurls,
 										  'results': results,
 										  'temp': temp})
+
+
+def urlparser(request):
+	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+	if request.method == 'POST':
+		allresult = Result.objects.all()
+		allurls = UrlTimer.objects.all()
+		templist = []
+		#collecting urls
+		for i in allresult:
+			templist.append(i.url)
+		for urls in allurls:
+			if not urls.remaining_time():
+				if urls not in templist:
+					resp = requests.get(urls.url, headers=headers)
+					if resp.status_code == 200:
+						soup = BS(resp.content, 'html.parser')
+						new_result = Result(url=urls, title=str(soup.title.text), encoding=str(soup.original_encoding), h1=str(soup.find('h1')))
+						new_result.save()
+			
+	return HttpResponseRedirect('/')
+
